58e3a0dc90f3fa5a2b7524c48d987b412218a6bc
Cristhian Rojas Soto <xcristhianx739@gmail.com>
2021-09-14 18:49:47 -0500

    Implementación pag de adopción

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Practica_02.Models;
+
+namespace Practica_02.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult FormAdopcion(){
+        return View();
+        }
+       [HttpPost]        
+       public IActionResult FormAdopcion(string nombre_completo, string tipo_mascota, string telefono, string distrito, string direccion)
+        {
+            return RedirectToAction("FormConfirmacion");
+        }
+        public IActionResult FormConfirmacion()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
